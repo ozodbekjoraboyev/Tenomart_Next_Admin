fcbbc14a25ab2c0b8d74051cb9814aabc8c77ff2
unknown <baxtiyor.gazimboyev@gmail.com>
2025-04-08 11:20:18 +0300

    s

--- src/app/ProduktCard.tsx
+++ src/app/ProduktCard.tsx
@@ -8,27 +8,33 @@ import Link from "next/link";
 
 function ProduktCard({ item }: { item?: CardsDataType }) {
   if (!item) {
-    return <div className="text-red-500">Mahsulot ma'lumotlari mavjud emas.</div>;
+    return (
+      <div className="text-red-500">Mahsulot ma'lumotlari mavjud emas.</div>
+    );
   }
 
   return (
     <div className="bg-white shadow-md rounded-lg p-4 max-w-[250px] mb-5">
-      {item.imageUrl ? (
-       <Link href={`/produktid/${item.id}`}> <Image
-       width={230}
-       height={250}
-       src={item.imageUrl}
-       alt={item.name || "Mahsulot"}
-       className="mx-auto rounded-lg object-cover"
-     /></Link>
-      ) : (
-        <div className="bg-gray-200 w-[230px] h-[250px] flex items-center justify-center rounded-lg">
-          <p className="text-gray-500">Rasm yo‘q</p>
-        </div>
-      )}
+      <Link href={`/produktid/${item.id}`}>
+        {item.imageUrl ? (
+          <Image
+            width={230}
+            height={360} // Yuqori aniqlikda bir xil hajmga kelishi uchun
+            src={item.imageUrl}
+            alt={item.name || "Mahsulot"}
+            className="mx-auto rounded-lg object-cover h-70 w-full"
+          />
+        ) : (
+          <div className="bg-gray-200 w-full h-40 flex items-center justify-center rounded-lg">
+            <p className="text-gray-500">Rasm yo‘q</p>
+          </div>
+        )}
+      </Link>
 
       <div className="mt-4">
-        <p className="text-gray-700 text-sm">{item.description || "Tavsif mavjud emas"}</p>
+        <p className="text-gray-700 text-sm line-clamp-2">
+          {item.description || "Tavsif mavjud emas"}
+        </p>
         <div className="flex justify-between items-center mt-3">
           <p className="text-lg font-semibold text-gray-900">
             {item.price ? `$${item.price}` : "Narx noma’lum"}
